5814ed4e39aa53c966c00cdcd5e929e8d4b7da35
yinjixian <yinjixian@picooc.com>
2017-03-20 11:38:54 +0800

    优化代码

---
+++ app/src/main/java/picooc/com/changelanguage/SettingActivity.java
@@ -0,0 +1,13 @@
+package picooc.com.changelanguage;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_setting);
+	}
+}
